d66d53e2640460211ada47e200bc61ae7d42056c
drzel <shayolden@hotmail.com>
2016-04-01 03:40:32 +1100

    use sensible and consistent variable and function names

--- problem22.rb
+++ problem22.rb
@@ -3,29 +3,29 @@
 
 class NameCalc
   # Return the alphabetical score of a letter. E.g. a = 1, b = 2, c = 3.
-  def alphabetical_score(letter)
+  def letter_score(letter)
     ('A'..'Z').to_a.index(letter.upcase).to_i + 1
   end
 
   # Return the aphabetical score of a string. E.g. 'abc' = 1 + 2 + 3 = 6.
-  def score_word(word)
+  def word_score(word)
     word.split('').inject(0) do |sum, letter|
-      sum + alphabetical_score(letter)
+      sum + letter_score(letter)
     end
   end
 
   def initialize(file)
     # Create a sorted array of names from text file by removing delimiters.
-    @name_list = File.read(file).delete('"').split(',').sort
+    @names = File.read(file).delete('"').split(',').sort
   end
 
   # Return total score by finding the sum of each word's score multiplied by
   # its position in the list.
   def score
     total_score = 0
-    @name_list.each_with_index do |word, index|
+    @names.each_with_index do |name, index|
       word_position = index + 1
-      name_score = score_word(word) * word_position
+      name_score = word_score(name) * word_position
       total_score += name_score
     end
     total_score
