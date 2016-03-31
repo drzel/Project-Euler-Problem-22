e9a4fd72a8e2e656083da98e2e2b4371c07718a5
drzel <shayolden@hotmail.com>
2016-04-01 02:07:05 +1100

    add score_word function

--- problem22.rb
+++ problem22.rb
@@ -4,3 +4,9 @@ ary = text.gsub!('"','').split(",").sort
 def alphabetical_score(letter)
   ("a".."z").to_a.index(letter) + 1
 end
+
+def score_word(word)
+  word.split("").inject(0) do |sum, letter|
+    sum + alphabetical_score(letter)
+  end
+end
