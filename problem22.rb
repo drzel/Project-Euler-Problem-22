644073d80309b1d5d17466a1fef178b6ce5744d6
drzel <shayolden@hotmail.com>
2016-04-01 02:32:35 +1100

    success!

--- problem22.rb
+++ problem22.rb
@@ -2,7 +2,7 @@ text = File.read "p022_names.txt"
 ary = text.gsub!('"','').split(",").sort
 
 def alphabetical_score(letter)
-  ("a".."z").to_a.index(letter).to_i + 1
+  ("A".."Z").to_a.index(letter).to_i + 1
 end
 
 def score_word(word)
@@ -13,6 +13,8 @@ end
 
 sum = 0
 ary.each_with_index do |word, index|
-  sum += score_word(word) * (index + 1)
+  word_position = index + 1
+  name_score = score_word(word) * word_position
+  sum += name_score
 end
 p sum
