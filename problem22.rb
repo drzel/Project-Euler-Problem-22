189bd1ea82c991b7c30a5e02c5ccf96b1188d4b1
drzel <shayolden@hotmail.com>
2016-04-01 02:52:48 +1100

    refactoring, minor fixes, added comments

--- problem22.rb
+++ problem22.rb
@@ -1,20 +1,28 @@
-text = File.read "p022_names.txt"
-ary = text.gsub!('"','').split(",").sort
+# Ruby exercise. Solution to Project Euler Problem 22.
+# https://projecteuler.net/problem=22
 
+# Return the alphabetical score of a letter. E.g. a = 1, b = 2, c = 3.
 def alphabetical_score(letter)
-  ("A".."Z").to_a.index(letter).to_i + 1
+  ('A'..'Z').to_a.index(letter.upcase).to_i + 1
 end
 
+# Return the aphabetical score of a string. E.g. 'abc' = 1 + 2 + 3 = 6.
 def score_word(word)
-  word.split("").inject(0) do |sum, letter|
+  word.split('').inject(0) do |sum, letter|
     sum + alphabetical_score(letter)
   end
 end
 
-sum = 0
+# Create a sorted array of names from text file by removing delimiters.
+ary = File.read('p022_names.txt').delete!('"').split(',').sort
+
+# Calculate total score by finding the sum of each word's score multiplied by
+# its position in the list.
+total_score = 0
 ary.each_with_index do |word, index|
   word_position = index + 1
   name_score = score_word(word) * word_position
-  sum += name_score
+  total_score += name_score
 end
-p sum
+
+p total_score
