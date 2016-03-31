61d349a97c5d3b678eca63cf94d5af7456111824
drzel <shayolden@hotmail.com>
2016-04-01 02:18:29 +1100

    completed with incorrect answer, time to debug

--- problem22.rb
+++ problem22.rb
@@ -2,7 +2,7 @@ text = File.read "p022_names.txt"
 ary = text.gsub!('"','').split(",").sort
 
 def alphabetical_score(letter)
-  ("a".."z").to_a.index(letter) + 1
+  ("a".."z").to_a.index(letter).to_i + 1
 end
 
 def score_word(word)
@@ -10,3 +10,9 @@ def score_word(word)
     sum + alphabetical_score(letter)
   end
 end
+
+sum = 0
+ary.each_with_index do |word, index|
+  sum += score_word(word) * (index + 1)
+end
+p sum
